698b06a25a7676492fd217fd073b50bf46f60f3e
abdulqadir <cajiibxaaji02@gmail.com>
2022-06-23 07:10:07 +0300

    React Chart

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,21 +1,52 @@
-import React from 'react'
-import Header from './Header'
+import React from "react";
+import { useState, useEffect } from "react";
+import Header from "./Header";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { Bar } from "react-chartjs-2";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 const Home = () => {
+  const [chartData, setChartData] = useState({
+    datasets: [],
+  });
+  const [chartOptions, setChartOptions] = useState({});
+
+  ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    BarElement,
+    Title,
+    Tooltip,
+    Legend
+  );
+
   return (
-    <div className='p-4'>
-      <Header/>
-      <div className='mt-5'>
+    <div className="p-4">
+      <Header />
+      <div className="mt-5">
         <h1>Overview</h1>
-        <div className="grid grid-cols-4 gap-12 mt-4">
-          <span className='px-4 py-10 rounded-md bg-white'>Col 1</span>
-          <span className='px-4 py-10 rounded-md bg-white'>Col 2</span>
-          <span className='px-4 py-10 rounded-md bg-white'>Col 3</span>
-          <span className='px-4 py-10 rounded-md bg-white'>Col 4</span>
+        <div className="" style={{ width: 700 }}>
+        <Bar options={chartOptions} data={chartData} />
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
